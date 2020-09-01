018895def129e6284a98df21870e0284e8dfc368
Rahul <saihanumarahul66@gmail.com>
2020-09-01 10:28:23 +0530

    external modules which are used is listed in installtions.py

---
+++ installations.py
@@ -0,0 +1,10 @@
+import os
+# TODO: for installing PIL package (image related package)
+os.system('pip install pillow')
+os.system('pip install --upgrade pip')
+
+#TODO: For installing pyglet package that can add .ttf files (font files) for adding the custom fonts
+os.system('pip install pyglet')
+
+#TODO: For installing google translate API 
+os.system('pip install googletrans')
