71b34dac2dda67f28ecf776512106a3270727314
Rahul <saihanumarahul66@gmail.com>
2020-09-01 15:16:21 +0530

    Added a export option in google translate API handler script

--- Raw Modules/Google Translate API/safecheckpoint.py
+++ Raw Modules/Google Translate API/safecheckpoint.py
@@ -2,12 +2,14 @@ import googletrans
 class GT:
     def __init__(self):
         self.lines=['']
+        self.from_,self.to_=None,None
         self.origin,self.translated,self.pronunciation=str(),str(),str()
     def get_Input(self,file_import=None,text=None):
         if file_import is not None:
             try:
                 hand=open(file_import,'r')
                 text=hand.read()
+                hand.close()
             except:
                 # ? For GUI use a dialog box here
                 print('Selected File is Not Supported For now..')
@@ -15,12 +17,37 @@ class GT:
         self.lines=text.split('\n')
     def translate(self,from_='en',to_='ja'):
         translator=googletrans.Translator()
+        self.from_,self.to_=from_,to_
         for i in self.lines:
             result=translator.translate(i,src=from_,dest=to_)
             self.origin+=result.origin+'\n'
             self.pronunciation+=result.pronunciation+'\n'
             self.translated+=result.text+'\n'
-
+    def export(self):
+        # TODO: Script inside this method is used to design the output text file
+        # ? use save as filedialog to save the exported file
+        # * Raw Modules\Google Translate API\test2.txt
+        filename=input('Enter the File Name: ')
+        
+        hand=open(filename,'wb')
+        hand.write('{} Text: \n'.format(self.from_).encode())
+        check=len('{} Text:'.format(self.from_))
+        hand.write('{}\n\n'.format('═'*check).encode())
+        hand.write(self.origin.encode())
+        hand.write('\n'.encode())
+        
+        hand.write('Translated to: {} \n'.format(self.to_).encode())
+        check=len('Translated to: {}'.format(self.to_))
+        hand.write('{}\n\n'.format('═'*check).encode())
+        hand.write(self.translated.encode())
+        hand.write('\n'.encode())
+        
+        hand.write('Pronunciation for the {} text is: \n'.format(self.to_).encode())
+        check=len('Pronunciation for the {} text is:'.format(self.to_))
+        hand.write('{}\n\n'.format('═'*check).encode())
+        hand.write(self.pronunciation.encode())
+        hand.write('\n'.encode())
+        hand.close()
 if __name__=='__main__':
     test=GT()
     test.get_Input(text='Hello there\n How Are You?')
@@ -44,3 +71,4 @@ if __name__=='__main__':
     print(test.translated)
     # TODO: Label
     print(test.pronunciation)
+    test.export()
