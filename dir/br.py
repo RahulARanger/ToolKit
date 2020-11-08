7ea389595a53815c1094b18d6dd827446c94629d
mp329 <65363996+mp329@users.noreply.github.com>
2020-11-08 19:48:21 +0530

    rahul babaseer

--- dir/br.py
+++ dir/br.py
@@ -21,7 +21,7 @@ SEARCH = StringVar()
 
 def Database():
     global conn, cursor
-    conn = sqlite3.connect("admin.db")
+    conn = sqlite3.connect("Resources\\Database\\admin.db")
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS `admin` (admin_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
     cursor.execute("CREATE TABLE IF NOT EXISTS `product` (product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, product_name TEXT, product_qty TEXT, product_price TEXT)")
